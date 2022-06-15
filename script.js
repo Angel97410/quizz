aa27073b409647895010f4062b8f8aecc92b6f2c
Angélique Robert <angel97410.ar@gmail.com>
2022-06-15 23:19:58 +0200

    création const + function verif

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const form = document.querySelector('.form-quizz');
 let tabResult = [];
 const reponse = ['c', 'a', 'b','a','c'];
+const emoji = ['✅','✨','👀','😭','👎']
+const titreResult = document.querySelector('.result h2');
+const textResult = document.querySelector('.note');
+const aideResult = document.querySelector('.aide');
+const allQuestion = document.querySelectorAll('.question-bloc');
+let verifTab = [];
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -9,5 +15,19 @@ form.addEventListener('submit', (e) => {
     tabResult.push(document.querySelector(`input[name="q${i}"]:checked`).value);
     console.log(tabResult)
   }
+  verifFunc(tabResult);
   tabResult = [];
 })
+
+function verifFunc(tabResult){
+
+  for (let a=0 ; a<5; a++){
+    if (tabResult[a]=== reponse[a]){
+      verifTab.push(true);
+    }else{
+      verifTab.push(false);
+    }
+  }
+  console.log(verifTab);
+  verifTab = [];
+}
